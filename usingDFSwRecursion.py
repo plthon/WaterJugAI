659e7ac8191b7cffd1a5bdf0ab49d6cea2e78070
bnthnix <101209471+bnthnix@users.noreply.github.com>
2019-10-16 20:15:04 +0800

    Made accept user input.

--- usingDFSwRecursion.py
+++ usingDFSwRecursion.py
@@ -1,28 +1,10 @@
-# 3 water jugs capacity -> (x,y,z) where x>y>z
-# initial state (12,0,0)
-# final state (6,6,0)
-
-
-capacity = (10, 6, 5)  # TODO
-# Maximum capacities of 3 jugs -> x,y,z
-x = capacity[0]
-y = capacity[1]
-z = capacity[2]
-
-# to mark visited states
-memory = {}
-
-# store solution path
-ans = []
-
-
 def get_all_states(state):
     # Let the 3 jugs be called a,b,c
     a = state[0]
     b = state[1]
     c = state[2]
 
-    if a == 10 and b == 6 and c == 5:  # TODO
+    if a == X and b == Y and c == Z:
         ans.append(state)
         return True
 
@@ -39,26 +21,26 @@ def get_all_states(state):
             ans.append(state)
             return True
         # fill a
-        if a < x:
-            if get_all_states((x, b, c)):
+        if a < B1:
+            if get_all_states((B1, b, c)):
                 ans.append(state)
                 return True
         # empty a into b
-        if a + b <= y:
+        if a + b <= B2:
             if get_all_states((0, a + b, c)):
                 ans.append(state)
                 return True
         else:
-            if get_all_states((a - (y - b), y, c)):
+            if get_all_states((a - (B2 - b), B2, c)):
                 ans.append(state)
                 return True
         # empty a into c
-        if a + c <= z:
+        if a + c <= B3:
             if get_all_states((0, b, a + c)):
                 ans.append(state)
                 return True
         else:
-            if get_all_states((a - (z - c), b, z)):
+            if get_all_states((a - (B3 - c), b, B3)):
                 ans.append(state)
                 return True
 
@@ -68,26 +50,26 @@ def get_all_states(state):
             ans.append(state)
             return True
         # fill a
-        if b < y:
-            if get_all_states((a, y, c)):
+        if b < B2:
+            if get_all_states((a, B2, c)):
                 ans.append(state)
                 return True
         # empty b into a
-        if a + b <= x:
+        if a + b <= B1:
             if get_all_states((a + b, 0, c)):
                 ans.append(state)
                 return True
         else:
-            if get_all_states((x, b - (x - a), c)):
+            if get_all_states((B1, b - (B1 - a), c)):
                 ans.append(state)
                 return True
         # empty b into c
-        if b + c <= z:
+        if b + c <= B3:
             if get_all_states((a, 0, b + c)):
                 ans.append(state)
                 return True
         else:
-            if get_all_states((a, b - (z - c), z)):
+            if get_all_states((a, b - (B3 - c), B3)):
                 ans.append(state)
                 return True
 
@@ -97,39 +79,75 @@ def get_all_states(state):
             ans.append(state)
             return True
         # fill a
-        if c < z:
-            if get_all_states((a, b, z)):
+        if c < B3:
+            if get_all_states((a, b, B3)):
                 ans.append(state)
                 return True
         # empty c into a
-        if a + c <= x:
+        if a + c <= B1:
             if get_all_states((a + c, b, 0)):
                 ans.append(state)
                 return True
         else:
-            if get_all_states((x, b, c - (x - a))):
+            if get_all_states((B1, b, c - (B1 - a))):
                 ans.append(state)
                 return True
         # empty c into b
-        if b + c <= y:
+        if b + c <= B2:
             if get_all_states((a, b + c, 0)):
                 ans.append(state)
                 return True
         else:
-            if get_all_states((a, y, c - (y - b))):
+            if get_all_states((a, B2, c - (B2 - b))):
                 ans.append(state)
                 return True
 
     return False
 
 
-def main():
-    initial_state = (0, 0, 0)  # TODO
-    print("Starting work...\n")
-    get_all_states(initial_state)
-    ans.reverse()
-    for i in ans:
-        print(i)
+def gettingInput():
+    print("Please enter maximum capacity of bottles:")
+    B1 = int(input("B1: "))
+    B2 = int(input("B2: "))
+    B3 = int(input("B3: "))
 
+    print("\nPlease enter start state of bottles:")
+    x = int(input("B1: "))
+    y = int(input("B1: "))
+    z = int(input("B1: "))
+
+    print("\nPlease enter goal state of bottles:")
+    X = int(input("B1: "))
+    Y = int(input("B1: "))
+    Z = int(input("B1: "))
+
+    return B1, B2, B3, x, y, z, X, Y, Z
+
+
+def main():
+    initial_state = (x, y, z)
+    print("\nStarting work...\n")
+    get_all_states(initial_state)
+    if len(ans) == 0:
+        print("No Possible Solution!")
+    else:
+        ans.reverse()
+        for i in ans:
+            print(i)
+
+
+# 3 water jugs capacity -> (x,y,z) where x>y>z
+B1, B2, B3, x, y, z, X, Y, Z = gettingInput()
+capacity = (B1, B2, B3)
+# Maximum capacities of 3 jugs -> x,y,z
+B1 = capacity[0]
+B2 = capacity[1]
+B3 = capacity[2]
+
+# to mark visited states
+memory = {}
+
+# store solution path
+ans = []
 
 main()
