7c06e44469e3c68844143b05841630f845a9b983
plthon <plthon@outlook.com>
2019-10-17 21:59:47 +0800

    Test Git.

--- usingDFSwStack.py
+++ usingDFSwStack.py
@@ -231,6 +231,7 @@ def solution():
             if checkIfEncountered(currentStack):
                 print(currentStack)
                 keepsTrackOfCurrentLevel.push(currentLevel + 1)
+                xxx
 """
 
 def main():
