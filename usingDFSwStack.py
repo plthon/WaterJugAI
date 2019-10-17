aee0e58314bccbf4db4b0336789a8bd2db5b59de
bnthnix <101209471+bnthnix@users.noreply.github.com>
2019-10-17 22:20:47 +0800

    Test Git.

--- usingDFSwStack.py
+++ usingDFSwStack.py
@@ -231,7 +231,6 @@ def solution():
             if checkIfEncountered(currentStack):
                 print(currentStack)
                 keepsTrackOfCurrentLevel.push(currentLevel + 1)
-                xxx
 """
 
 def main():
